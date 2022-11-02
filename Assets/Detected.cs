2fa333837b571183aee88ad304df5465e9cfa4bf
Garrett <garwad9@gmail.com>
2022-11-02 17:12:06 -0500

    finished animations

---
+++ Assets/Detected.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detected : MonoBehaviour
+{
+    void Update()
+    {
+       private void OnTriggerEnter2D(Collider2D collider)
+        {
+            Debug.Log("Trigger");
+        } 
+    }
+}
