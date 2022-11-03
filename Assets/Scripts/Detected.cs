ad092e30a61d0b1e65cb09f804e8698582a77e5a
Garrett <garwad9@gmail.com>
2022-11-02 19:52:01 -0500

    Patrol

--- Assets/Scripts/Detected.cs
+++ Assets/Scripts/Detected.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Detected : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
+        if (other.tag == ("Player"))
+        {
+            Debug.Log("game over");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
